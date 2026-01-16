6591ab7f0d58318f40ae2698510a88fc53e35f42
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-16 16:35:16 +0530

    [Rishabh]. Add Program.cs to run all the practice problems in generics project.

---
+++ Generics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CSharp_Advance_Collections.Generics.SmartWarehouseManagementSystem;
+using CSharp_Advance_Collections.Generics.UniversityCourseManagementSystem;
+using CSharp_Advance_Collections.Generics.PersonalizedMealPlanGenerator;
+using CSharp_Advance_Collections.Generics.AI_DrivenResumeScreeningSystem;
+
+namespace CSharp_Advance_Collections.Generics
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            // SmartWareHouseExecutable.Execute();
+            
+            // UniversityCourseExecutable.Execute();
+
+            // MealExecutable.Execute();
+            
+            AI_DrivenResumeExecutable.Execute();
+        }
+    }
+}

27b817eb3beda82a73ff3ac70418980ab64335af
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-17 12:17:14 +0530

    [Rishabh]. add program.cs to run all the practice problems in collection_extra project.

---
+++ Collections_Extras/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CSharp_Advance_Collections.Collections_Extras.ChatMessageCleanupSystem;
+using CSharp_Advance_Collections.Collections_Extras.DriverAssignmentSystem;
+using CSharp_Advance_Collections.Collections_Extras.LibraryFineCalculation;
+using CSharp_Advance_Collections.Collections_Extras.BankTransactionFraudDetector;
+using CSharp_Advance_Collections.Collections_Extras.OnlineExamResultNormalisation;
+
+
+namespace CSharp_Advance_Collections.Collections_Extras
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            // ChatCleanupMain.Execute();
+
+            // CabAssignmentMain.Execute();
+
+            // FineCalculatorMain.Execute();
+
+            // FraudMain.Execute();
+
+            // ResultMain.Execute();
+        }
+    }
+}
